5467c695572716187b21d660967cc3945429f459
Richard Horton (hortonr6) <hortonr6@coventry.ac.uk>
2019-07-18 19:43:36 +0100

    Created Outline of LinkedList Class

--- LinkedList.py
+++ LinkedList.py
@@ -16,3 +16,26 @@ class Node(object):
 
     def setNext(self, newNext):
         self.nextNode = newNext
+
+class LinkedList(object):
+    def __init__(self, head = None):
+        self.head = head
+        self.nodeCount = 0
+
+    def getNodeCount(self):
+        return self.count
+
+    def insertNode(self, nodeData):
+        newNode = Node(data)
+
+    def searchList(self, target):
+        currentNode = self.head
+
+        return None
+
+    def deleteAt(self, index):
+        if index > self.count-1:
+            return
+
+    
+        
