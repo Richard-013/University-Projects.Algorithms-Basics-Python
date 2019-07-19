3ff5c7b20b02012d00fbe96afbf8925b20aecc7a
Richard Horton (hortonr6) <hortonr6@coventry.ac.uk>
2019-07-19 20:51:22 +0100

    Implemented Bubble Sort

--- Sorting.py
+++ Sorting.py
@@ -1,5 +1,17 @@
-def bubbleSort():
-    pass
+def bubbleSort(data):
+    for i in range(len(data), -1, -1):
+        for j in range(i - 1):
+            print("COMP:", j, end=' ')
+            if data[j] > data[j+1]:
+                temp = data[j+1]
+                data[j+1] = data[j]
+                data[j] = temp
+                print("SWAPPED")
+            else:
+                print("NOT SWAPPED")
+                continue
+
+    return data
 
 def mergeSort():
     pass
