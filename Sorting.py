fddddd8635a9780d94223e3803a0f9ec7b27c96a
Richard Horton (hortonr6) <hortonr6@coventry.ac.uk>
2019-07-19 21:11:51 +0100

    Implemented Merge Sort

--- Sorting.py
+++ Sorting.py
@@ -14,13 +14,53 @@ def bubbleSort(data):
 
     return data
 
-def mergeSort():
-    pass
+def mergeSort(data):
+    '''Sorts a list of elements using merge sort algorithm'''
+    if len(data) > 1:
+        #Splits list into two halves at the midpoint
+        midpoint = len(data) // 2
+        leftList = data[:midpoint]
+        rightList = data[midpoint:]
+
+        mergeSort(leftList)
+        mergeSort(rightList)
+
+        leftIndex = 0
+        rightIndex = 0
+        mergeIndex = 0
+
+        #Compares and merges both lists
+        while(leftIndex < len(leftList) and rightIndex < len(rightList)):
+            if leftList[leftIndex] < rightList[rightIndex]:
+                data[mergeIndex] = leftList[leftIndex]
+                leftIndex += 1
+            else:
+                data[mergeIndex] = rightList[rightIndex]
+                rightIndex += 1
+
+            mergeIndex += 1
+
+        #Adds in remaining elements from the left list
+        while leftIndex < len(leftList):
+            data[mergeIndex] = leftList[leftIndex]
+            leftIndex += 1
+            mergeIndex += 1
+
+        #Adds in remaining elements from the right list
+        while rightIndex < len(rightList):
+            data[mergeIndex] = rightList[rightIndex]
+            rightIndex += 1
+            mergeIndex += 1
+
+        print(data)
+        return data
+        
 
 def quickSort():
     pass
 
 inputData = [3, 8, 42, 9, 31, 12, 25]
 print(inputData)
-print(bubbleSort(inputData))
+#print(bubbleSort(inputData))
+print(mergeSort(inputData))
 
