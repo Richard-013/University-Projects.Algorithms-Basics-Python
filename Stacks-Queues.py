e497e61987d1b16ec5fa1b46ec509ba7ff781594
Richard Horton (hortonr6) <hortonr6@coventry.ac.uk>
2019-07-19 16:54:20 +0100

    Set Up Outline for Stack and Queue

---
+++ Stacks-Queues.py
@@ -0,0 +1,24 @@
+class Stack:
+    def __init__(self):
+        pass()
+
+    def push(self):
+        '''Pushes the given item to the top of the stack'''
+        pass()
+        
+    def pop(self):
+        '''Pops the item at the top of the stack'''
+        pass()
+
+class Queue:
+    def __init__(self):
+        pass()
+
+    def push(self):
+        '''Pushes the given item to the back of the queue'''
+        pass()
+
+    def pop(self):
+        '''Pops the item at the front of the queue'''
+        pass()
+    
