2385650a10f6b73c71698ae507362225ff2f17a5
Richard Horton (hortonr6) <hortonr6@coventry.ac.uk>
2019-07-22 12:03:11 +0100

    Implemented Linear Search with Boolean Result

--- Searching.py
+++ Searching.py
@@ -4,3 +4,11 @@ def linearSearch(target, data):
             return item
 
     return None
+
+def linearSearchBool(target, data):
+    for item in data:
+        if item == target:
+            return True
+
+    return False
+
