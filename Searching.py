696891e6bad4ff973b88f76a82ff33fc0c1633c3
Richard Horton (hortonr6) <hortonr6@coventry.ac.uk>
2019-07-22 12:03:23 +0100

    Tested Linear Search Implementations

--- Searching.py
+++ Searching.py
@@ -12,3 +12,16 @@ def linearSearchBool(target, data):
 
     return False
 
+inputData = [43, 67, 12, 34, 76, 4, 19, 7, 200, 38]
+
+#Linear Search Test
+#Standard
+print(linearSearch(43, inputData))
+print(linearSearch(19, inputData))
+print(linearSearch(569, inputData))
+
+print("-------")
+#Bool
+print(linearSearchBool(38, inputData))
+print(linearSearchBool(12, inputData))
+print(linearSearchBool(8, inputData))
